b981096b2df994b9c846dc513137f8a7cf3a9241
Lucas Morais <lucas.morais@bsc.es>
2023-01-20 17:18:09 +0100

    Implement random generation of both acyclic and possibly cyclic digraphs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use petgraph::graph::{NodeIndex, UnGraph};
+use petgraph::graph::{NodeIndex, UnGraph, DiGraph};
+use petgraph::graphmap::{GraphMap};
 use petgraph::algo::{dijkstra, min_spanning_tree};
 use petgraph::data::FromElements;
 use petgraph::dot::{Dot, Config};
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
+use rand::Rng;
 
 // Here I use the ampersend to prevent this function
 // from taking ownership of the string that is passed
@@ -42,16 +44,49 @@ fn gen_sample_graph() -> UnGraph<i32, ()> {
     return mst;
 }
 
-fn gen_random_graph() {
+// TODO: Add an option for ensuring that the graph is acyclic
+fn gen_random_digraph(acyclic: bool) -> GraphMap<i64, i64, petgraph::Directed> {
+    let mut g = GraphMap::<i64, i64, petgraph::Directed>::new();
+
+    let mut rng = rand::thread_rng();
+
+    let num_nodes: i32 = rng.gen_range(1..=20);
+    let max_num_edges = num_nodes * (num_nodes - 1) / 2;
+    let num_edges: i32 = rng.gen_range(0..=max_num_edges);
+
+    println!("Number of nodes: {}", num_nodes);
+    println!("Number of edges: {}", num_edges);
+
+    for i in 0..num_nodes {
+        g.add_node(i.into());
+    }
+
+    for i in 0..num_edges {
+        let mut a: i64 = 0;
+        let mut b: i64 = 0;
+
+        if !acyclic {
+            a = rng.gen_range(0..num_nodes).into();
+            b = rng.gen_range(0..num_nodes).into();
+        } else {
+            a = rng.gen_range(0..num_nodes - 1).into();
+            b = rng.gen_range(a+1..num_nodes.into()).into();
+        }
+
+        g.add_edge(a, b, i.into());
+    }
+
+    // The following is equivalent to 'return g;'
+    g
 }
 
 fn gen_graph_image(file_name: &'static str) {
-    let _dot_proc_output = Command::new("dot").arg("-Tpng").arg(file_name).arg("-o").arg("test.png").output().unwrap();
+    let _dot_proc_output = Command::new("dot").arg("-Tpng").arg(file_name).arg("-o").arg("output.png").output().unwrap();
 }
 
 fn gen_sample_graph_image() {
-    let g = gen_sample_graph();
-
+    //let g = gen_sample_graph();
+    let g = gen_random_digraph(true);
 
     // Output the tree to `graphviz` `DOT` format
     //
