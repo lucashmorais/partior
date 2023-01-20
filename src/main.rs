f116b6a283233bc4f0249b69a8d54d5d30b41be2
Lucas Morais <lucas.morais@bsc.es>
2023-01-20 13:49:58 +0100

    Add graph library dep; print sample .dot graph to stdout

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use petgraph::graph::{NodeIndex, UnGraph};
+use petgraph::algo::{dijkstra, min_spanning_tree};
+use petgraph::data::FromElements;
+use petgraph::dot::{Dot, Config};
+
+
+// Create an undirected graph with `i32` nodes and edges with `()` associated data.
+fn gen_sample_graph() -> UnGraph<i32, ()> {
+    let g = UnGraph::<i32, ()>::from_edges(&[
+        (1, 2), (2, 3), (3, 4),
+        (1, 4)]);
+
+    return g;
+}
+
+fn gen_sample_graph_image() {
+    let g = gen_sample_graph();
+
+    // Find the shortest path from `1` to `4` using `1` as the cost for every edge.
+    let node_map = dijkstra(&g, 1.into(), Some(4.into()), |_| 1);
+    assert_eq!(&1i32, node_map.get(&NodeIndex::new(4)).unwrap());
+
+    // Get the minimum spanning tree of the graph as a new graph, and check that
+    // one edge was trimmed.
+    let mst = UnGraph::<_, _>::from_elements(min_spanning_tree(&g));
+    assert_eq!(g.raw_edges().len() - 1, mst.raw_edges().len());
+
+    // Output the tree to `graphviz` `DOT` format
+    println!("{:?}", Dot::with_config(&mst, &[Config::EdgeNoLabel]));
+}
+
+fn main() {
+    gen_sample_graph_image();
+}
